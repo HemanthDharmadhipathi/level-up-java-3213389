7e4be9bc79aa2f9b3e9621b16d20318eb1b4b83d
HemanthDharmadhipathi <hemanthdharmadhipathi@gmail.com>
2023-02-01 09:36:42 +0000

    code.

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
+        if (password != null) {
+            boolean hasCapitalLetter = false;
+            boolean hasSmallLetter = false;
+            boolean hasNumber = false;
+            char[] c = password.toCharArray();
+            int length = c.length;
+            if (length >= 6) {
+                for (char p : c) {
+                    if (p >= 'a' && p <= 'z') {
+                        hasSmallLetter = true;
+                    } else if (p >= 'A' && p <= 'Z') {
+                        hasCapitalLetter = true;
+                    } else if (p >= '0' && p <= '9') {
+                        hasNumber = true;
+                    }
+
+                    if (hasCapitalLetter && hasSmallLetter && hasNumber) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
